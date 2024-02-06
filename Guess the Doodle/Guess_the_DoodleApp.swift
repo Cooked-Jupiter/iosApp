1b871f3c1771543d694af21edff11095d32390b4
Kohl <kkennedy@anva.com>
2024-02-06 14:52:18 -0800

    Initial Commit

---
+++ Guess the Doodle/Guess_the_DoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_DoodleApp.swift
+//  Guess the Doodle
+//
+//  Created by Work on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_DoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
